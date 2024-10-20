54e24164bd38916bab331387677506a61453e66b
Stefania Neri <stefania.neri.92@gmail.com>
2024-10-20 22:04:27 +0200

    .

--- src/app/curriculum/curriculum.component.ts
+++ src/app/curriculum/curriculum.component.ts
@@ -6,11 +6,10 @@ import { Component } from '@angular/core';
   styleUrl: './curriculum.component.css',
 })
 export class CurriculumComponent {
-  urlCV: string = '../../assets/CV new SF.pdf';
+  urlCV: string = 'assets/CV new SF.pdf';
 
   openCV() {
-    if(typeof window !== 'undefined'){
-
+    if (typeof window !== 'undefined') {
       window.open(this.urlCV, '_blank');
     }
   }
