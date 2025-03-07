bd3d9c4dda81e7abcc8d2396bfce575112b6f829
StefaniaNeri <stefania.neri.92@gmail.com>
2025-03-07 12:55:47 +0100

    changed hero

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-QSKMMQJ3.mjs";import"./chunk-EQRJCMAB.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-K2EOCQXD.mjs";import"./chunk-EQRJCMAB.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
