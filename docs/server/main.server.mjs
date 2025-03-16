c33c3822373557f149370504add15185d0e630d5
Stefania Neri <stefania.neri.92@gmail.com>
2025-03-16 19:54:48 +0100

    fixed projects & hero

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-H56TJ5WE.mjs";import"./chunk-EQRJCMAB.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-JOLYDDTS.mjs";import"./chunk-EQRJCMAB.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
