61a8959ad4ae3a4e956a633b144344823dc95cfb
Stefania Neri <stefania.neri.92@gmail.com>
2025-03-31 23:16:47 +0200

    fix form

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-Y5ZFHYFI.mjs";import"./chunk-FRXEKHHC.mjs";import"./chunk-K7YHPIF3.mjs";export{a as default};
+import{a}from"./chunk-BH6LDISD.mjs";import"./chunk-FI5IH5IN.mjs";import"./chunk-K7YHPIF3.mjs";export{a as default};
--- src/app/contacts/contacts.component.ts
+++ src/app/contacts/contacts.component.ts
@@ -11,36 +11,52 @@ export class ContactsComponent implements AfterViewInit {
   constructor(private toast: NgToastService) {}
 
   @ViewChild('myFormRef') myFormRef!: NgForm;
-  @ViewChild('fullName') fullName!: ElementRef;
-  @ViewChild('email') email!: ElementRef;
-  @ViewChild('message') message!: ElementRef;
+  // @ViewChild('fullName') fullName!: ElementRef;
+  // @ViewChild('email') email!: ElementRef;
+  // @ViewChild('message') message!: ElementRef;
+
+  fullName: string = '';
+  email: string = '';
+  message: string = '';
+  showErrors: boolean = false;
+  invalidEmail: boolean = false;
+
+  isValidEmail(){
+    if(this.email && this.email.includes('@')){
+      return true
+    }
+  }
 
   ngAfterViewInit() {
    
   }
 
   async sendForm() {
-    try {
+    if(!this.fullName || !this.isValidEmail() || !this.message) {
+      this.showErrors = true;
+      return
+    }
 
-      const formElement = document.querySelector('form') as HTMLFormElement;
+    try {
+          const formElement = document.querySelector('form') as HTMLFormElement;
       const response = await fetch(formElement.action, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
         },
         body: JSON.stringify({
-          fullName: this.fullName.nativeElement.value,
-          email: this.email.nativeElement.value,
-          message: this.message.nativeElement.value
+          fullName: this.fullName,
+          email: this.email,
+          message: this.message
         })
       });
 
       const data = await response.json();
       console.log("Risposta dal server:", data);
-      // window.location.href = "https://stefanianeri.github.io/portfolio/#contact";
-      this.myFormRef.reset();
       this.showSuccess();
-      
+      this.myFormRef.reset();
+      this.showErrors = false;
+
     } catch (error) {
       console.error("Errore nell'invio del form:", error);
       this.showError();
@@ -49,8 +65,7 @@ export class ContactsComponent implements AfterViewInit {
 
 
   showSuccess() {
-    this.toast.success('Message sent successfully!', '', 6000);
-    
+    this.toast.success(`Thank you, ${this.fullName}. Your message has been sent!`, '', 6000);
   }
 
   showError() {
