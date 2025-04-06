781eefe710f4fe76fe7901f72cbc9e1070cd172f
Stefania Neri <stefania.neri.92@gmail.com>
2025-04-06 22:00:12 +0200

    fixes

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-35WVRMQI.mjs";import"./chunk-FI5IH5IN.mjs";import"./chunk-K7YHPIF3.mjs";export{a as default};
+import{a}from"./chunk-CWIDLRPA.mjs";import"./chunk-FI5IH5IN.mjs";import"./chunk-K7YHPIF3.mjs";export{a as default};
