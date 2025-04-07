f4e9f5954da765bd0469f81d8cde2639e1fa0b7c
Stefania Neri <stefania.neri.92@gmail.com>
2025-04-07 22:57:56 +0200

    fixed layout responsive

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-4JQMD3DQ.mjs";import"./chunk-FI5IH5IN.mjs";import"./chunk-K7YHPIF3.mjs";export{a as default};
+import{a}from"./chunk-P4QKQ6Y2.mjs";import"./chunk-FI5IH5IN.mjs";import"./chunk-K7YHPIF3.mjs";export{a as default};
